0e0e3025f8e1bd8fa58910d0bfb82fa6446f388b
Wonil Suh <wonil.suh1@ibm.com>
2017-10-09 18:20:18 -0400

    - console log remobved

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import getDuration from './getDuration';
 import getMotion from './getMotion';
 import constants from './constants.js';
 
-console.log('@ibm/motion/index.js!');
-
 export default {
 	getCurve,
 	getDuration,
