e63779ad8ce5bb2d72c5a298808a9e02084adef6
Wonil Suh <wonil.suh1@ibm.com>
2018-02-17 16:46:15 -0500

    - description is back in

--- src-demo/views/MainView/SiteHeader.js
+++ src-demo/views/MainView/SiteHeader.js
@@ -7,25 +7,30 @@ class SiteHeader extends React.Component{
 
 	render(){
 		return(
-			<div 
-				className="SiteHeader"
-				style={{
-					height:'48px',
-					display:'flex',
-					alignItems:'center',
-					justifyContent:'space-between'
-				}}
-			>
+			<div>
 				<div 
-					className="ibm-type-d padding-h"
-					 style={{
-						paddingTop:0,
-						paddingBottom:0,
-						margin:0
+					className="SiteHeader"
+					style={{
+						height:'48px',
+						display:'flex',
+						alignItems:'center',
+						justifyContent:'space-between'
 					}}
-				>IBM Motion Generator</div>
-				<div className="padding-h">
-					<a target="_blank" href="https://github.com/IBM/motion">IBM Motion Codebase</a>
+				>
+					<div 
+						className="ibm-type-d padding-h"
+						 style={{
+							paddingTop:0,
+							paddingBottom:0,
+							margin:0
+						}}
+					>IBM Motion Generator</div>
+					<div className="padding-h">
+						<a target="_blank" href="https://github.com/IBM/motion">IBM Motion Codebase</a>
+					</div>
+				</div>
+				<div className="ibm-type-b padding-h" style={{maxWidth:'40em'}}>
+					Duo Motion comprises several unique concepts - use this tool to get the right motion parameters for your element.
 				</div>
 			</div>
 		)
